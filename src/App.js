4ffdc272dd7e7ae9d7fc6ad3bd7817f4f87f203b
Unknown <rezkyauliapratama@gmail.com>
2018-09-25 00:08:38 +0700

    Lecture-44

--- src/App.js
+++ src/App.js
@@ -14,10 +14,10 @@ class App extends Component {
     otherState: 'some other value'
   }
 
-  switchNameHandler = () =>{
+  switchNameHandler = (newName) =>{
       this.setState({
         persons: [
-          {name : 'Maximilan', age : 28},
+          {name : newName, age : 28},
           {name : 'Andree', age : 22},
           {name : 'Chika', age : 22}
         ]
@@ -29,10 +29,17 @@ class App extends Component {
       <div className="App">
        <h1>Hi, I'm react app</h1>
        <p>This is really working !</p>
-       <button onClick={this.switchNameHandler}>Switch name</button>
-       <Person name ={this.state.persons[0].name} age={this.state.persons[0].age}/>
-       <Person name ={this.state.persons[1].name} age={this.state.persons[1].age}/>
-       <Person name ={this.state.persons[2].name} age={this.state.persons[2].age}/>
+       <button onClick={()=>this.switchNameHandler("Maximilian")}>Switch name</button>
+       <Person 
+        name ={this.state.persons[0].name} 
+        age={this.state.persons[0].age}/>
+       <Person 
+        name ={this.state.persons[1].name} 
+        age={this.state.persons[1].age}
+        click={this.switchNameHandler.bind(this,"Rezky Aulia")}/>
+       <Person 
+        name ={this.state.persons[2].name} 
+        age={this.state.persons[2].age}/>
        
 
       </div>
