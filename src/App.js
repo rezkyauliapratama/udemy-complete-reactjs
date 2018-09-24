c4af4dd55a83a76e98e36ad4148477903b6cc43c
Unknown <rezkyauliapratama@gmail.com>
2018-09-24 23:36:53 +0700

    Lecture-36

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ class App extends Component {
       <div className="App">
        <h1>Hi, I'm react app</h1>
        <p>This is really working !</p>
-       <Person/>
-       <Person/>
-       <Person/>
+       <Person name ="Max" age="28"/>
+       <Person name ="Andree" age="22">my hobbies : racing</Person>
+       <Person name ="Chika" age="17"/>
 
       </div>
     );
